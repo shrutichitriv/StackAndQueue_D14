c8d5a595eefc09c7ef5bef080181b6e536eb59b4
shrutichitriv <shrutichitriv199620@gmail.com>
2022-12-07 21:00:44 +0530

    Stacks and Queues Programs

---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack1
+
+            Stack1 s = new Stack1();//Create Stack Object
+            s.Add(70);
+            s.Add(30);
+            s.Add(56);
+            s.Display();//Call Display Method
+        }
+    }
+}
